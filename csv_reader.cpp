338a0c2c653e2ff06361e1530e3aae71576569e3
Rudra Nil Basu <rudra.n.basu@gmail.com>
2020-07-29 18:36:56 +0530

    ParseCSV: Compress parser to ParseCSV

--- csv_reader.cpp
+++ csv_reader.cpp
@@ -1,11 +1,11 @@
+/*
+ * - single file library?
+ * - struct for csv storage?
+ */
+
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 #include <stdint.h>
-#include <float.h>
-#include <math.h>
-#include <time.h>
-#include <assert.h>
 
 #define internal static
 #define global static
@@ -29,6 +29,12 @@ typedef double f64;
 
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
 
+enum Type
+{
+    Type_Source,
+    Type_Compare,
+};
+
 b32 Hash[10000000] = {};
 
 internal b32
@@ -39,44 +45,14 @@ CheckIfPresent(u32 HashId)
     return(Result);
 }
 
-int main(int ArgCount, char **Args)
+internal void
+ParseCSV(FILE *CsvFile, FILE *OutFile, Type CsvFileType)
 {
-    printf("CSV Reader\n");
-
-    if (ArgCount != 4)
-    {
-        printf("Error: Usage: ./csv_reader <source_filename> <compare_to_filename> <output_file_name>\n");
-        return(1);
-    }
-
-    FILE *SourceCsvFile;
-    FILE *ToCompareCsvFile;
-    // CsvFile = fopen("test.csv", "r");
-    SourceCsvFile = fopen(Args[1], "r");
-    ToCompareCsvFile = fopen(Args[2], "r");
-    if (SourceCsvFile == NULL)
-    {
-        printf("Unable to open file: %s\n", Args[1]);
-        return(0);
-    }
-    if (ToCompareCsvFile == NULL)
-    {
-        printf("Unable to open file: %s\n", Args[2]);
-        return(0);
-    }
-
-    FILE *OutFile = fopen(Args[3], "wb");
-    if(!OutFile)
-    {
-        fprintf(stderr, "[ERROR] Unable to open output file %s.\n", Args[3]);
-    }
-
-    // TODO(@rudra): Compress!
     char LineBuffer[1024];
     u32 LineNumber = 0;
-    while (!feof(SourceCsvFile))
+    while (!feof(CsvFile))
     {
-        if (fgets(LineBuffer, ArrayCount(LineBuffer)-1, SourceCsvFile) == NULL)
+        if (fgets(LineBuffer, ArrayCount(LineBuffer)-1, CsvFile) == NULL)
         {
             break;
         }
@@ -92,7 +68,6 @@ int main(int ArgCount, char **Args)
             continue;
         }
 
-        // printf("Line: %s\n", LineBuffer);
         u32 HashId = 0;
         for (u32 Index = 0;
              Index < ArrayCount(LineBuffer);
@@ -108,123 +83,69 @@ int main(int ArgCount, char **Args)
             }
             HashId = HashId*10 + (LineBuffer[Index]-'0');
         }
-        Hash[HashId] = 1;
-        // CheckIfPresent(HashId);
-    }
 
-    // Checker
-    // char LineBuffer[1024];
-    LineNumber = 0;
-    while (!feof(ToCompareCsvFile))
-    {
-        if (fgets(LineBuffer, ArrayCount(LineBuffer)-1, ToCompareCsvFile) == NULL)
+        if (CsvFileType == Type_Source)
         {
-            break;
+            Hash[HashId] = 1;
         }
-
-        if (++LineNumber == 1)
+        else if (CsvFileType == Type_Compare)
         {
-            // printf("Skipping first line as header\n");
-            continue;
-        }
-        if (LineBuffer[0] == '\n')
-        {
-            // printf("Skipping blank lines\n");
-            continue;
-        }
-
-        // printf("Line: %s\n", LineBuffer);
-        u32 HashId = 0;
-        for (u32 Index = 0;
-             Index < ArrayCount(LineBuffer);
-             Index++)
-        {
-            if (LineBuffer[Index] == 0)
+            if (!CheckIfPresent(HashId))
             {
-                break;
-            }
-            if (LineBuffer[Index] == ',')
-            {
-                break;
-            }
-            HashId = HashId*10 + (LineBuffer[Index]-'0');
-        }
-        if (!CheckIfPresent(HashId))
-        {
-            printf("Not present: %d\n", HashId);
-
-            u32 LineLength = 0;
-            for (u32 Index = 0;
-                 Index < ArrayCount(LineBuffer);
-                 Index++)
-            {
-                LineLength++;
-                if (LineBuffer[Index] == '\n')
+                u32 LineLength = 0;
+                for (u32 Index = 0;
+                     Index < ArrayCount(LineBuffer);
+                     Index++)
                 {
-                    break;
+                    LineLength++;
+                    if (LineBuffer[Index] == '\n')
+                    {
+                        break;
+                    }
                 }
+                // 1 => sizeof(LineBuffer[0])
+                fwrite(&LineBuffer, sizeof(LineBuffer[0])*LineLength, 1, OutFile);
             }
-            // fwrite(&LineBuffer, sizeof(LineBuffer), 1, OutFile);
-            fwrite(&LineBuffer, sizeof(LineBuffer[0])*LineLength, 1, OutFile);
         }
+
     }
+}
+
+int main(int ArgCount, char **Args)
+{
+    printf("CSV Reader\n");
+
+    if (ArgCount != 4)
+    {
+        fprintf(stderr, "[ERROR] Usage: ./csv_reader [SOURCE_CSV] [COMPARE_TO_CSV] [OUTPUT_CSV]\n");
+        return(1);
+    }
+
+    FILE *SourceCsvFile;
+    FILE *ToCompareCsvFile;
+    SourceCsvFile = fopen(Args[1], "r");
+    ToCompareCsvFile = fopen(Args[2], "r");
+    if (SourceCsvFile == NULL)
+    {
+        fprintf(stderr, "[ERROR] Unable to open file: %s\n", Args[1]);
+        return(0);
+    }
+    if (ToCompareCsvFile == NULL)
+    {
+        fprintf(stderr, "[ERROR] Unable to open file: %s\n", Args[2]);
+        return(0);
+    }
+
+    FILE *OutFile = fopen(Args[3], "wb");
+    if(!OutFile)
+    {
+        fprintf(stderr, "[ERROR] Unable to open output file %s.\n", Args[3]);
+    }
+
+    ParseCSV(SourceCsvFile, OutFile, Type_Source);
+    ParseCSV(ToCompareCsvFile, OutFile, Type_Compare);
 
     fclose(SourceCsvFile);
     fclose(ToCompareCsvFile);
     fclose(OutFile);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // fp = fopen(Args[1], "r");
-
-    /*
-    char Buffer[1024];
-    size_t Count = fread(&Buffer, sizeof(Buffer[0]), ArrayCount(Buffer), CsvFile);
-    */
-
-    // printf("CLOCKS_PER_SECOND: %d\n", CLOCKS_PER_SECOND);
-    /*
-    printf("CLOCKS_PER_SECOND: %d\n", CLOCKS_PER_SEC); // 1000 clocs per sec: millisec cloc
-
-
-    while(Queue.TileRetiredCount < TotalTileCount)
-    {
-        if (RenderTile(&Queue))
-        {
-            fprintf(stderr, "\rRaycasting %d%%", (100 * (u32)Queue.TileRetiredCount / TotalTileCount));
-            fflush(stdout);
-        }
-    }
-
-    clock_t EndClock = clock();
-    clock_t TimeElapsed = EndClock - StartClock;
-    fprintf(stderr, "\n");
-    printf("Raycasting time: %d ms\n", TimeElapsed);
-    printf("Total Bounces: %llu\n", Queue.BouncesComputed);
-    if((f64)Queue.BouncesComputed != 0)
-    {
-        printf("Performance:  %fms/ bounce\n", (f64)TimeElapsed / (f64)Queue.BouncesComputed);
-    }
-
-    WriteImage(Image, "test.bmp");
-
-    fprintf(stderr, "\nDone\n");
-
-    return(0);
-    */
